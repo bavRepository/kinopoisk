52e9ac33bcd9f329be037cdeae653d3a6d1d717e
Anton <a.barai6003@gmail.com>
2025-11-19 11:55:07 +0300

    Added popular movies fetch

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -4,30 +4,46 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
   refetchOnReconnect: true,
-  baseQuery: async (args, api, extraOptions) => {
-    await new Promise<void>((resolve) => {
-      setTimeout(() => {
-        resolve()
-      }, 1200)
-    })
-    const res = await fetchBaseQuery({
-      // await fetchBaseQuery({
-      baseUrl: import.meta.env.VITE_BASE_URL,
-      credentials: 'include',
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
-        'API-KEY': import.meta.env.VITE_API_KEY,
-        accept: 'application/json',
-      },
-      prepareHeaders: (headers) => {
-        headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-      },
-    })(args, api, extraOptions)
+  // baseQuery: async (args, api, extraOptions) => {
+  //   await new Promise<void>((resolve) => {
+  //     setTimeout(() => {
+  //       resolve()
+  //     }, 1200)
+  //   })
+  //   const res = await fetchBaseQuery({
+  //     // await fetchBaseQuery({
+  //     baseUrl: import.meta.env.VITE_BASE_URL,
+  //     credentials: 'include',
+  //     headers: {
+  //       Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+  //       'API-KEY': import.meta.env.VITE_API_KEY,
+  //       accept: 'application/json',
+  //     },
+  //     prepareHeaders: (headers) => {
+  //       headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
+  //     },
+  //   })(args, api, extraOptions)
+  //
+  //   // handleError(api, res)
+  //
+  //   return res
+  // },
+  baseQuery: fetchBaseQuery({
+    // await fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    credentials: 'include',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+      'API-KEY': import.meta.env.VITE_API_KEY,
+      accept: 'application/json',
+    },
+    prepareHeaders: (headers) => {
+      headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
+    },
+  }),
 
-    // handleError(api, res)
+  // handleError(api, res)
 
-    return res
-  },
   // Через сколько обновлять кэш keepUnusedDataFor : 120 можно убрать из глобального и оставить конкретному запросу
   // refetchOnFocus: true чтобы в компаненте сделать локально надо   const { data, isLoading } = useGetTasksQuery({id, params: {page: page}}, {refetchOnFocus: true})
   reducerPath: 'baseApi',
