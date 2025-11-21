c4fe688c1e6dbea8289923d5bedd436a7647030d
Anton <a.barai6003@gmail.com>
2025-11-21 12:42:12 +0300

    Added linear progress

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -2,16 +2,17 @@ import { Routing } from '@/common/routing'
 import { Header } from '@/common/components'
 import s from './app.module.css'
 import { ToastContainer } from 'react-toastify'
-// import { useGlobalLoading } from '@/common/hooks/useGlobalLoading.ts'
-// import { LinearProgress } from '@/common/components/LinearProgress/LinearProgress.tsx'
 import { useEffect, useState } from 'react'
 import { Footer } from '@/common/components/Footer/Footer.tsx'
 import { restoreState } from '@/common/localStorage/localStorage.ts'
 import { useAppDispatch } from '@/common/hooks'
 import { changeThemeModeAC } from '@/app/model/app-slice.ts'
 import { SkeletonTheme } from 'react-loading-skeleton'
+import { useGlobalLoading } from '@/common/hooks/useGlobalLoading.ts'
+import { LinearProgress } from '@/common/components/LinearProgress/LinearProgress.tsx'
 
 export const App = () => {
+  const isGlobalLoading = useGlobalLoading()
   const [loaded, setLoaded] = useState(false)
   const dispatch = useAppDispatch()
   const theme = restoreState('light')
@@ -25,6 +26,7 @@ export const App = () => {
     <SkeletonTheme baseColor={'#c1c0c8'} highlightColor={'#E5E7EB'}>
       <div className={`${s.contentWrapper} ${loaded ? s.loaded : ''}`}>
         <Header />
+        {isGlobalLoading && <LinearProgress />}
         {/*{isGlobalLoading && <LinearProgress />}*/}
         <Routing />
         <ToastContainer />
