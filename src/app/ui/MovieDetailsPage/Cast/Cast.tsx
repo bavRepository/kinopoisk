a92bac830074104df0b8a2ac2a59cb56d80d3b0a
Anton <a.barai6003@gmail.com>
2025-12-04 18:41:49 +0300

    Fixed skeleton view in the movie details

--- src/app/ui/MovieDetailsPage/Cast/Cast.tsx
+++ src/app/ui/MovieDetailsPage/Cast/Cast.tsx
@@ -18,21 +18,21 @@ export const Cast = ({ currentTheme, getImg, castMemberListCut, isSimilarLoading
         <h1 className={s.castHeader + colorNightClass}>Cast</h1>
       ) : null}
       <div className={s.castContent}>
-        {isSimilarLoading && (
+        {isSimilarLoading ? (
           <SkeletonMovie options={{ round: true, style: { borderRadius: 999, width: 180, height: 180 } }} />
-        )}
-
-        {castMemberListCut?.map((cast) => {
-          return (
-            <div key={cast.id} className={s.castCard}>
-              <img src={getImg(cast?.profile_path)} alt='#' className={s.castImg} />
-              <div className={s.castImgDescWrapper}>
-                <p className={s.castName + colorNightClass}>{cast.name}</p>
-                <p className={s.castRole}>{cast.character}</p>
+        ) : (
+          castMemberListCut?.map((cast) => {
+            return (
+              <div key={cast.id} className={s.castCard}>
+                <img src={getImg(cast?.profile_path)} alt='#' className={s.castImg} />
+                <div className={s.castImgDescWrapper}>
+                  <p className={s.castName + colorNightClass}>{cast.name}</p>
+                  <p className={s.castRole}>{cast.character}</p>
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })
+        )}
       </div>
     </div>
   )
--- src/app/ui/MovieDetailsPage/Similar/Similar.tsx
+++ src/app/ui/MovieDetailsPage/Similar/Similar.tsx
@@ -2,6 +2,7 @@ import s from './similar.module.css'
 import { MovieCategoryModel } from '@/common/MovieCategoryModel/MovieCategoryModel.tsx'
 import type { BaseMoviesResponse, SimilarMovieWithFavoriteType } from '@/features/movies/api/moviesApi.types.ts'
 import type { ThemeMode } from '@/app/model/app-slice.ts'
+import { SkeletonMovie } from '@/common/components/Skeleton/SkeletonMovie.tsx'
 
 type PropsType = {
   similarMovieResponse: BaseMoviesResponse<SimilarMovieWithFavoriteType[]> | undefined
@@ -9,7 +10,7 @@ type PropsType = {
   isSimilarLoading: boolean
   currentTheme: ThemeMode
 }
-export const Similar = ({ similarMovieResponse, id, isSimilarLoading, currentTheme }: PropsType) => {
+export const Similar = ({ similarMovieResponse, id, currentTheme, isSimilarLoading }: PropsType) => {
   const colorNightClass = currentTheme === 'dark' ? ' ' + s.night : ''
   return (
     <div className={s.similarMovies}>
@@ -17,15 +18,18 @@ export const Similar = ({ similarMovieResponse, id, isSimilarLoading, currentThe
         <h1 className={s.similarMoviesHeader + colorNightClass}>Similar Movies</h1>
       ) : null}
       <div className={s.similarMoviesContent}>
-        <MovieCategoryModel
-          movies={similarMovieResponse?.results}
-          options={{
-            full: false,
-            skeleton: true,
-            params: id,
-          }}
-          isLoading={isSimilarLoading}
-        />
+        {isSimilarLoading ? (
+          <SkeletonMovie options={{ round: false }} />
+        ) : (
+          <MovieCategoryModel
+            movies={similarMovieResponse?.results}
+            options={{
+              full: false,
+              skeleton: true,
+              params: id,
+            }}
+          />
+        )}
       </div>
     </div>
   )
