63bc0b6c150355cffc071825e4e4500f7207bc29
Anton <a.barai6003@gmail.com>
2025-12-04 19:15:40 +0300

    Fixed linear progress bar activity

--- src/common/hooks/useGlobalLoading.ts
+++ src/common/hooks/useGlobalLoading.ts
@@ -3,7 +3,12 @@ import { useSelector } from 'react-redux'
 import { moviesApi } from '@/features/movies/api/moviesApi.ts'
 
 // Список эндпоинтов для исключения из глобального индикатора
-const excludedEndpoints: any = [moviesApi.endpoints.getSearchMovie.name]
+const excludedEndpoints: any = [
+  moviesApi.endpoints.getSearchMovie.name,
+  moviesApi.endpoints.getGenres.name,
+  moviesApi.endpoints.getMovieConfiguration.name,
+  moviesApi.endpoints.getMovieListByFilter.name,
+]
 export const useGlobalLoading = () => {
   return useSelector((state: RootState) => {
     const queries = Object.values(state.baseApi.queries || {})
